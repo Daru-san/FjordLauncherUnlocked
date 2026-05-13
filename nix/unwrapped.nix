6fdee7c5c5281f8881ace829f543539b98f11abd
Daru <mica.tui@proton.me>
2026-05-13 12:11:16 +0200

    refactor: pull extra-cmake-modules build input from kdePackages

--- nix/unwrapped.nix
+++ nix/unwrapped.nix
@@ -3,7 +3,6 @@
   stdenv,
   cmake,
   cmark,
-  extra-cmake-modules,
   gamemode,
   jdk17,
   kdePackages,
@@ -64,7 +63,7 @@ stdenv.mkDerivation {
   nativeBuildInputs = [
     cmake
     ninja
-    extra-cmake-modules
+    kdePackages.extra-cmake-modules
     jdk17
     stripJavaArchivesHook
   ];
